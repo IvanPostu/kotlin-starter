92864219a6811ec832a5394bd308ec65ebcdbf04
IvanPostu <ipostu20000127@gmail.com>
2025-07-05 21:03:54 +0300

    add ktor and create a simple server app

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ group = "com.iv127.kotlin.starter"
 version = "1.0-SNAPSHOT"
 
 application {
-    mainClass.set("com.iv127.kotlin.starter.MainKt")
+    mainClass.set("com.iv127.kotlin.starter.Main")
 }
 
 repositories {
@@ -19,6 +19,8 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation(kotlin("stdlib-jdk8"))
+    implementation("io.ktor:ktor-server-core:2.1.2")
+    implementation("io.ktor:ktor-server-netty:2.1.2")
 }
 
 tasks.test {
--- src/main/kotlin/com/iv127/kotlin/starter/Main.kt
+++ src/main/kotlin/com/iv127/kotlin/starter/Main.kt
@@ -1,10 +1,32 @@
 package com.iv127.kotlin.starter;
 
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
 class Main {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            println("Hello, World! Class=" + Main.javaClass.name)
+            embeddedServer(Netty, port = 4207) {
+                createKtorApplication()
+            }.start(wait = true)
         }
+
+        private fun Application.createKtorApplication() {
+            routing {
+                get("/") {
+                    call.respondText(getClicheMessage())
+                }
+            }
+        }
+
+
+        private fun getClicheMessage(): String {
+            return "Hello, World! Class=" + Main.javaClass.name
+        }
+
     }
 }
