113bbe302a00adc47f5c59e33980fff592a62dca
IvanPostu <ipostu20000127@gmail.com>
2025-07-06 01:24:00 +0300

    make headers case insensitive

--- src/main/kotlin/com/iv127/kotlin/starter/WebResponse.kt
+++ src/main/kotlin/com/iv127/kotlin/starter/WebResponse.kt
@@ -8,6 +8,18 @@ sealed class WebResponse {
         headers: Map<String, List<String>>
     ): WebResponse
 
+    fun headers(): Map<String, List<String>> =
+        headers
+            .map { it.key.lowercase() to it.value }
+            .fold(mapOf()) { res, (k, v) ->
+                res.plus(
+                    Pair(
+                        k,
+                        res.getOrDefault(k, listOf()).plus(v)
+                    )
+                )
+            }
+
     fun header(headerName: String, headerValue: String) = header(headerName, listOf(headerValue))
 
     fun header(headerName: String, headerValues: List<String>) = copyResponse(
